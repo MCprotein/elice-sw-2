c98bc1a13ca25a25a3a720c26c8780a7c4815a04
MCprotein <chss3339@gmail.com>
2022-05-16 17:08:42 +0900

    blog 게시글 조회, 등록

--- 0509/afternoon/myapp/routes/blog.js
+++ 0509/afternoon/myapp/routes/blog.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const blogSchema = require('../models/blog');
 
-router.get('/', (req, res) => {
-    res.render('blog/blog');
+router.get('/', async (req, res) => {
+    const result = await blogSchema.find({}).exec();
+    res.render('blog/blog', {content: result});
 });
 
 router.get('/write', (req, res) => {
     res.render('blog/write');
-})
+});
+
+router.post('/write', (req, res) => {
+    const title = req.body.title;
+    const content = req.body.content;
+
+    const blogText = new blogSchema({
+        title: title,
+        content: content,
+    });
+
+    blogText.save().then(result => {
+        console.log(result);
+        res.redirect('/blog');
+    }).catch(err => {
+        console.log(err);
+        next(err);
+    })
+});
 
 //localhost:3000/blog
 
