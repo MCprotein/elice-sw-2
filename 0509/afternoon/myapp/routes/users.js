559b0fd10148780ff951333edd9da2f20c2e882d
MCprotein <chss3339@gmail.com>
2022-05-18 11:38:39 +0900

    비밀번호 암호화

--- 0509/afternoon/myapp/routes/users.js
+++ 0509/afternoon/myapp/routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
+const userSchema = require('../models/newuser');
+const bcrypt = require('bcrypt');
+const {body, validationResult} = require('express-validator');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('blog/auth');
+});
+
+// 정규 표현식으로 인증
+//passport
+
+router.post('/signup'
+        , body('email').isEmail().withMessage('아이디는 email 형태를 따르셔야 합니다.')
+        , body('password').isLength({min:5}).withMessage('비밀번호는 최소 5글자 이상입니다.')
+        , (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array()
+    });
+  }
+
+  const salt = bcrypt.genSaltSync(10);
+  const bcryptpw = bcrypt.hashSync(req.body.password, salt);
+  // 복호화 할때 기준이 되는 메세지의 길이가 존재함.
+
+  userSchema.create({
+    email:req.body.email,
+    password:bcryptpw
+  }).then(result => {
+    res.status(200).json(result);
+  });
 });
 
 module.exports = router;
