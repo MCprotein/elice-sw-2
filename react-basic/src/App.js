f7c0529de880a66a3fc27dcb47cfee56ee24c0af
chss3339 <chss3339@gmail.com>
2022-06-06 14:45:11 +0900

    react 배포와 컴포넌트

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,23 +1,51 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+function HeaderTag() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>
+        <a href="/">Webㄴㄴ</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  console.log(props.data);
+  const list = props.data.map((e) => {
+    return (
+      <li key={e.id}>
+        <a href={"/read/" + e.id}>{e.title}</a>
+      </li>
+    );
+  });
+  return (
+    <nav>
+      <ol>{list}</ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
+function App() {
+  const topics = [
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+  ];
+  return (
+    <div>
+      <HeaderTag></HeaderTag>
+      <Nav data={topics}></Nav>
+      <Article title="welcome ho" body="Hello, WEB!"></Article>
     </div>
   );
 }
